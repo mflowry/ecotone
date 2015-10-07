1d8b3fa7b8355f46b2f36a41ef34b16a21c64c6a
kstoffregen <stoffrk@Kathryns-MacBook-Pro.local>
2015-10-07 11:48:31 -0500

    basic layout

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,8 @@
-app =
+var app = angular.module('calculatorApp', ['ngMaterial']);
+
+app.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('green')
+        .accentPalette('blue')
+        .warnPalette ('orange');
+});
