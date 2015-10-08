a6d29abe8f0f57a4b29375ed696cd1be73ad3fb2
James McElwain <jmcelwain@gmail.com>
2015-10-08 18:44:03 -0500

    all user crud unit tests passing

--- server/routes/public/login.js
+++ server/routes/public/login.js
@@ -5,12 +5,12 @@ const
 var Users = require('../../models/users');
 
 router.post('/', function(req, res, next) {
-  Users.getAuthenticated(req.body, function (err, token) {
+  Users.getAuthenticated(req.body, function (err, token, user) {
     if (err) {
       console.log('ERROR',err.message);
       res.status(400).send(err.message);
     } else {
-      res.send(token);
+      res.send({token: token, user: user});
     }
   });
 });
--- test/routes.js
+++ test/routes.js
@@ -13,6 +13,7 @@ const
  */
 
 describe('A user', function(){
+
     var newUser = {
        username: chance.last(),
        password: 'test',
@@ -39,4 +40,41 @@ describe('A user', function(){
             .send(newUser)
             .expect(409, done);
     });
+
+    var token;
+    var user;
+
+    it('should authorize a user', function( done ){
+
+
+        var loginUser = {
+            username: newUser.username,
+            password: newUser.password
+        };
+
+        api.post('/login')
+            .send(loginUser)
+            .end(function( err, res ){
+                res.body.should.have.property('user');
+                res.body.should.have.property('token');
+                token = res.body.token;
+                user = res.body.user;
+                done();
+            })
+    });
+
+    it('should update a user', function( done ){
+
+        var newName = chance.last();
+        api.put('/modifyUser')
+            .set('Authorization', 'Bearer ' + token)
+            .send({firstName: newName})
+            .expect(200, done);
+    });
+
+    it('should delete a user', function( done ){
+        api.delete('/modifyUser/' + user.id)
+            .set('Authorization', 'Bearer ' + token)
+            .expect(200, done);
+    })
 });
