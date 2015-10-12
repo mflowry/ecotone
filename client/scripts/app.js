305255b4b4bb9500da5f30dc94b16fec7ae8c5dd
James McElwain <jmcelwain@gmail.com>
2015-10-12 17:22:44 -0500

    ctrlizing 3

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -51,61 +51,29 @@ app.config(['$mdThemingProvider', '$routeProvider', '$locationProvider', '$httpP
     //$httpProvider.interceptors.push('authInterceptor');
 }]);
 
-    //M//designate controller
 app.controller('calculateCtrl', ['$scope', '$http', function($scope, $http) {
-// create object to send to backend for calculation
-
-    $scope.saveToProject = function(){
-        var lineItem = {
-            category: $scope.category,
-            subcategory: $scope.subcategory,
-            warm_Id: $scope.warmId,
-            weight: parseFloat($scope.weight)*$scope.conversion,
-            units: $scope.unit.name
-        };
-        console.log(lineItem);
-        $http.post('/addToProject').then(function(response) {
-            console.log(response);
-            });
-        };
-
-    $scope.newCalculation = function(){
-        console.log("Calculating...", $scope.weight);
-        var calculate = {
-            warmId: $scope.warmId || $scope.category.secondaries[0].warm_id,
-            weight: parseFloat($scope.weight)*$scope.conversion
-        };
-        console.log(calculate);
-        $http.post('/calculations', calculate).then(function(response) {
-            $scope.result = Math.floor(Math.abs(response.data) * 1000) / 1000;
-            console.log($scope.result);
-
-        });
-    };
-
-//autocomplete functionality
-    $scope.querySearch=function(query) {
-        // console.log($scope.list.filter(createFilterFor(query)));
-        return query ? $scope.list.filter(createFilterFor(query)) : $scope.list;
-    };
-
-//load Primary categories list on page load
+    // INIT
     $http.get('/materials').then(function(response) {
         var list = response.data;
 
         list.forEach(function(item){
-            //item.primary_cat = item.primary_cat.toLowerCase();
-
-            // causing bugs right now
-            item.primary_cat = item.primary_cat.charAt(0).toUpperCase() + item.primary_cat.slice(1).toLowerCase();
+            item.primary_cat = item.primary_cat.toLowerCase();
+            // /item.primary_cat = item.primary_cat.charAt(0).toUpperCase() + item.primary_cat.slice(1).toLowerCase();
 
         });
-        $scope.list = list;
+        self.list = list;
 
     });
 
-//load the units
-    $scope.units = [
+    // Self dec
+    var self = this;
+
+    self.list = '';
+    self.result = '';
+    self.querySearch = querySearch;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange   = searchTextChange;
+    self.units = [
         {
             name: 'lbs',
             conversion: 0.0005
@@ -123,19 +91,60 @@ app.controller('calculateCtrl', ['$scope', '$http', function($scope, $http) {
             conversion: 1.10231
         }
     ];
-//
+    self.newCalculation = newCalculation;
 
-//Create filter function for a query string
+
+    function searchTextChange(text) {
+        console.log('Text changed to ', text);
+    }
+
+    function selectedItemChange(item) {
+        if ( item == undefined ) {
+            self.category = '';
+            self.subcategory = '';
+            self.warmId = '';
+            self.weight = '';
+            self.conversion = '';
+        }
+    }
+
+    function newCalculation() {
+
+        var calculate = {
+            warmId: self.warmId || self.category.secondaries[0].warm_id,
+            weight: parseFloat(self.weight) * self.conversion
+        };
+
+        $http.post('/calculations', calculate).then(function(response) {
+            self.result = Math.floor(Math.abs(response.data) * 1000) / 1000;
+        });
+    }
+
+    function saveToProject(){
+        var lineItem = {
+            category: self.category,
+            subcategory: self.subcategory,
+            warm_Id: self.warmId,
+            weight: parseFloat(self.weight)*self.conversion,
+            units: self.unit.name
+        };
+        console.log(lineItem);
+        $http.post('/addToProject').then(function(response) {
+            console.log(response);
+        });
+    }
+
+    function querySearch(query) {
+        return query ? self.list.filter(createFilterFor(query)) : self.list;
+    }
+
     function createFilterFor(query) {
-        var lowercaseQuery = query.charAt(0).toUpperCase() + query.slice(1);
-        //console.log(query);
+        var lowercaseQuery = query.toLowerCase() //query.charAt(0).toUpperCase() + query.slice(1);
         return function filterFn(obj) {
-            //console.log(obj.primary_cat);
             return (obj.primary_cat.indexOf(lowercaseQuery) != -1);
         };
     }
 }]);
-
 
 // Login HTML - Kate
 app.controller('loginCtrl', ['$scope', '$http', 'authService', function($scope, $http, authService) {
