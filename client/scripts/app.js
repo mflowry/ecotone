a384541f39b1f047c9217afa54a542b3ce33e814
Madeleine Lowry <mlowry55403@gmail.com>
2015-10-13 18:41:00 -0500

    project page js started

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -34,11 +34,11 @@ app.config(['$mdThemingProvider', '$routeProvider', '$locationProvider', '$httpP
         }).when('/dashboard',
         {
             templateUrl: '/views/dashboard.html',
-            controller: 'projectsCtrl'
+            controller: 'dashboardCtrl'
         }).when('/projects',
         {
             templateUrl: '/views/project.html',
-            controller: 'projectsCtrl'
+            controller: 'projectsCtrl as ctrl'
         }).when('/about',
         {
             templateUrl: '/views/about.html'
@@ -266,28 +266,73 @@ app.controller('navCtrl', ['authService', '$scope', '$rootScope', '$location', '
     }
 }]);
 
-// Project HTML - Dashboard HTML - Kim
+// Project Page -  Kim/Madeleine
 app.controller('projectsCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('/project').then(function (response) {
-        console.log(response);
-        $scope.projectList = response.data;
-        //response.data.forEach(function(item){
-        //    item.project_name = item.primary_cat.toLowerCase();
-    });
 
-    $scope.deleteProjectItem = function (lineID) {
-        console.log("Deleting...", $scope.projectlist.lineID);
-        $http.update('/project').then(function (response) {
-            //findOne and delete by ID using projectlist.lineID
-            console.log("Item was deleted.", response);
+    // Self dec
+    var self = this;
+    self.projectList = '';
+    self.result = '';
+    self.querySearch = querySearch;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange = searchTextChange;
+    self.projectTotal= 0;
+    self.deleteProjectItem = deleteProjectItem;
+    self.id = 0;
+
+
+    //load project list on page load
+    self.getProjectList = function() {
+        //need to figure out how to pass the userid from the login page--use the same controller?
+        $http.get('/project', userid).then(function (response) {
+            console.log(response);
+            var projectList = response.data;
+            projectList.forEach(function (item) {
+                item.project_name = item.project_name.toLowerCase();
+                item.project_description = item.project_description;
+                item.id = item.id;
+            });
+            self.projectList = projectList;
         });
-        //$scope.calculateProjectTotal(){
+    };
+
+    function querySearch(query) {
+        return query ? self.projectList.filter(createFilterFor(query)) : self.projectList;
+    }
+
+    function createFilterFor(query) {
+        var lowercaseQuery = query.toLowerCase();
+        //query.charAt(0).toUpperCase() + query.slice(1);
+        return function filterFn(obj) {
+            return (obj.primary_cat.indexOf(lowercaseQuery) != -1);
+        };
+    }
+
+    function searchTextChange(text) {
+        console.log('Text changed to ', text);
+    }
+
+    function selectedItemChange(item) {
+        if ( item == undefined ) {
+            self.project_name = '';
+            self.project_description = '';
+        }
+    }
+
+        function deleteProjectItem() {
+        console.log("Deleting...", self.projectList.ID);
+        //$http.update('/project').then(function (response) {
+        //findOne and delete by id using projectlist.id
+        //    console.log("Item was deleted.", response);
+        //    self.getProjectList();
+        };
+
+        //self.calculateProjectTotal(){
         ////forEach project
         //    var projectTotal+=projectList.co2_offset;
         //    return projectTotal;
-        ////save to project? or just recalc each time?
         //}
-    };
+    //};
 }]);
 
 
