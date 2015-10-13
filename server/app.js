333fc22b6a64e1d149b572707511ee7dd070973e
James McElwain <jmcelwain@gmail.com>
2015-10-13 09:50:51 -0500

    fixed reload

--- server/app.js
+++ server/app.js
@@ -42,7 +42,7 @@ app.use('/modifyUser',modifyUser);
 app.use('/calculations',calculations);
 app.use('/project',project);
 app.use('/suggestions', suggestion);
-
+app.use('*', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
