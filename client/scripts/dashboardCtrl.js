f0350a025f157aafd8024b3284aca04bb0349dc3
Kim <kimhayden@Kims-MacBook-Air.local>
2015-10-14 09:46:50 -0500

    getting kim's work back from stash

--- client/scripts/dashboardCtrl.js
+++ client/scripts/dashboardCtrl.js
@@ -1,5 +1,4 @@
-//Dashboard Page - Kim/Madeleine
-app.controller('dashboardCtrl', ['$scope', '$http', function($scope, $http) {
+app.controller('dashboardCtrl', ['$mdDialog', '$scope', '$http', function($mdDialog, $scope, $http) {
     //load project list on page load
     $http.get('/project').then(function(response) {
         console.log(response);
@@ -7,4 +6,34 @@ app.controller('dashboardCtrl', ['$scope', '$http', function($scope, $http) {
         //response.data.forEach(function(item){
         //    item.project_name = item.primary_cat.toLowerCase();
     });
+//this is for the delete modal alert for deleting a project-- still not working correct
+
+    $scope.showDelete = function(ev) {
+
+        console.log('CLICK');
+        var confirm = $mdDialog.confirm()
+            .title('Are you sure you want to delete your project?')
+            .content('This is permanent.')
+            .ariaLabel('Delete project permanently')
+            .ok('Delete')
+            .cancel('Cancel')
+            .targetEvent(ev);
+        $mdDialog.show(confirm).then(function() {
+            $scope.alert = 'Your project has been deleted.';
+        }, function() {
+            $scope.alert = 'You project has not been deleted.';
+        });
+    };
+//    function DialogController($scope, $mdDialog) {
+//    $scope.hide = function() {
+//        $mdDialog.hide();
+//    };
+//    $scope.cancel = function() {
+//        $mdDialog.cancel();
+//    };
+//    $scope.answer = function(answer) {
+//        $mdDialog.hide(answer);
+//    };
+//}
+
 }]);
