f7aaaad15c48d3ae6780ab4e6f728e5afa11630b
James McElwain <jmcelwain@gmail.com>
2015-10-15 09:19:30 -0500

    bulk stuff

--- server/routes/public/bulkUpload.js
+++ server/routes/public/bulkUpload.js
@@ -1,9 +1,31 @@
 const
     express = require('express'),
     router = express.Router();
+    pg = require('pg');
+
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/ecotone';
+
 
 router.post('/', function(req, res){
     console.log(req.body);
+    pg.connect( connectionString , function( err, client , done){
+        if( err ){
+            console.log(err);
+        } else {
+            console.log(warmID);
+            client.query('select co2 from proxies where id=$1', [warmID], function(err, results){
+                console.log(results);
+                if(results){
+                    calculation = parseFloat(results.rows[0].co2)*weight;
+                    res.json(calculation);
+                } else{
+                    res.send("Cannot find that proxy");
+                }
+            });
+            done();
+        }
+    });
+
     res.sendStatus(200);
 
 });
