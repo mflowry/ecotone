4e19795523a5de2148df8496f84829ee940eff0b
Madeleine Lowry <mlowry55403@gmail.com>
2015-10-21 16:17:35 -0500

    total co2 calc dashbd

--- client/scripts/dashboardCtrl.js
+++ client/scripts/dashboardCtrl.js
@@ -8,6 +8,11 @@ app.controller('dashboardCtrl', ['$location', 'projectMethods', '$mdDialog', '$r
     self.Co2GrandTotal = 0;
     self.getAllItems= getAllItems;
     self.calculateGrandTotal= calculateGrandTotal;
+    self.calculateProjectTotal = calculateProjectTotal;
+    self.projects.id = '';
+    self.projectItems = '';
+    self.projectTotal = 0;
+
 
     $scope.showDelete = function(project) {
         console.log('CLICK');
@@ -35,14 +40,14 @@ app.controller('dashboardCtrl', ['$location', 'projectMethods', '$mdDialog', '$r
     };
 
 
-    // get the active projects
+// get the active projects
     projectMethods.getProjectNames(function (names) {
         self.projects = names;
-        console.log(names);
+        console.log("Proj name: ", names);
     });
     getAllItems();
 
-
+//select a project to edit
     function selectProject(project){
         console.log(project);
         projectMethods.setSelectedProject(project);
@@ -50,17 +55,36 @@ app.controller('dashboardCtrl', ['$location', 'projectMethods', '$mdDialog', '$r
         $location.path('/projects');
     }
 
+//get all calculations for all projects to calc Grand Total CO2
     function getAllItems() {
-        projectMethods.getAllProjectItems().then(function( items ){
-            self.selectItems = items;
-            calculateGrandTotal();
+    projectMethods.getAllProjectItems().then(function( items ){
+        self.selectItems = items;
+        calculateGrandTotal();
         });
     }
 
+    //calculate the Grand Total
     function calculateGrandTotal() {
-
-            self.selectItems.forEach(function (item) {
-                self.Co2GrandTotal += item.co2_offset;
+        self.selectItems.forEach(function (item) {
+        self.Co2GrandTotal += item.co2_offset;
             });
+        }
+
+    //calculate co2 for each project
+    function getProjectItemsbyProjectId(id) {
+        self.projects.forEach(function () {
+            projectMethods.getProjectItems(function (items) {
+                self.projectItems = items;
+                console.log(items);
+            });
+            calculateProjectTotal();
+        })
+    }
+
+    //calculate project total co2
+    function calculateProjectTotal(){
+        self.projectItems.forEach(function (item) {
+        self.projectTotal += item.co2_offset;
+            })
         }
 }]);
