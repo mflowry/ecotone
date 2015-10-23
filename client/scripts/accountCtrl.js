609f4e6eb362b9d9670946227350761d656616f6
Kathryn Stoffregen <kathryn.stoffregen@gmail.com>
2015-10-23 14:19:45 -0500

    forgot password added to account page

--- client/scripts/accountCtrl.js
+++ client/scripts/accountCtrl.js
@@ -19,4 +19,26 @@ app.controller('editAccountCtrl', ['$scope', '$http', '$location', '$rootScope',
         });
     };
 
+    $scope.submitReset = function () {
+        $http.post('/forgot', {email: this.email}).then(function( res ){
+            showToast.showToast('An email with password reset instructions has been sent.');
+            $mdDialog.hide();
+        }, function(err){
+            console.log(err);
+            showToast.showToast(err.data.message);
+            $mdDialog.hide();
+        });
+    };
+
+    $scope.resetPassword = function() {
+
+        $mdDialog.show({
+            templateUrl: '/views/reset-modal.html',
+            clickOutsideToClose: true,
+            controller: 'loginCtrl',
+            controllerAs: 'ctrl'
+        })
+
+    }
+
 }]);
