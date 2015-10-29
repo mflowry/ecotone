f00ed68ce76080f4e9230566ad37a29920850d4f
James McElwain <jmcelwain@gmail.com>
2015-10-29 10:35:47 -0500

    redir working

--- server/app.js
+++ server/app.js
@@ -23,7 +23,8 @@ const
     suggestion = require('./routes/private/suggestion'),
     bulk = require('./routes/public/bulkUpload'),
     forgot = require('./routes/public/forgot'),
-    reset = require('./routes/public/reset');
+    reset = require('./routes/public/reset'),
+    wildcard = require('./routes/wildcard');
 
 var app = express();
 
@@ -46,8 +47,8 @@ app.use(cookieParser());
 app.use(expressValidator({customValidators: customValidators}));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/reset', reset);
 app.use('/', routes);
+app.use('/reset', reset);
 app.use('/login', login);
 app.use('/materials', materials);
 app.use('/register',register);
@@ -57,7 +58,7 @@ app.use('/project',project);
 app.use('/suggestion', suggestion);
 app.use('/bulk', bulk);
 app.use('/forgot', forgot);
-//app.use('*', routes);
+app.use('/*', wildcard);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/wildcard.js
+++ server/routes/wildcard.js
@@ -1,11 +1,17 @@
+const
+    express = require('express'),
+    router = express.Router();
 
-router.get('/*', function(req, res, next){
-  var url = req.url;
-  if (url.split('.').length > 1){
-    next(); // this should be handled by your 404 handler, optionally use res.sendStatus(404);
-  } else {
-    // handles angular urls. i.e. anything without a '.' in the url (so files aren't handled)
-    console.log('Catch all handled url: ' + req.originalUrl);
-    res.redirect('/#' + req.url);
-  }
+router.get('/*', function (req, res, next) {
+    var url = req.originalUrl;
+    if (url.split('.').length > 1) {
+        next(); // this should be handled by your 404 handler, optionally use res.sendStatus(404);
+    } else {
+        // handles angular urls. i.e. anything without a '.' in the url (so files aren't handled)
+        var newurl = '/#' + url;
+        console.log('Catch all handled url: ' + req.originalUrl, 'New url: ' + newurl);
+        res.redirect(newurl);
+    }
 });
+
+module.exports = router;
